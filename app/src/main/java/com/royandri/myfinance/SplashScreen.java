4d531908858e1a912994edf6b49a18d0aabcd1e0
royandri <royandri502@gmail.com>
2019-04-09 23:52:36 +0700

    MyFinance

---
+++ app/src/main/java/com/royandri/myfinance/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.royandri.myfinance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        img = (ImageView) findViewById(R.id.logo);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.transition);
+        img.startAnimation(anim);
+        final Intent i = new Intent(this,HomeScreen.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
